0eff27c2dc6850a5d973ff1ddb6d231deda8ea25
Ben Babik <work@benjaminbabik.com>
2020-08-21 00:55:24 +0100

    init

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,26 @@
+const autoPreprocess = require('svelte-preprocess');
+
+module.exports = ({ config, mode }) => {
+    const svelteLoader = config.module.rules.find(
+        r => r.loader && r.loader.includes('svelte-loader'),
+    );
+    svelteLoader.options.preprocess = autoPreprocess({
+        less: { includePaths: ['src', 'node_modules'] },
+        css: { includePaths: ['src', 'node_modules'] },
+        typescript: {
+            tsconfigFile: './tsconfig.json',
+            transpileOnly: true,
+        },
+    });
+    config.module.rules.push({
+        include: /\.(ts|tsx)$/,
+        exclude: /\.(spec.ts|test.ts)$/,
+        use: [
+            {
+                loader: require.resolve('ts-loader'),
+            },
+        ],
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+};
